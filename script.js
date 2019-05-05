d528d712ce0c9ba50c24ffbfd31a88cbadacf351
Jordan Hamilton <jordan-hamilton@users.noreply.github.com>
2019-05-04 21:06:59 -0600

    Update table size, update border size on click

--- script.js
+++ script.js
@@ -13,7 +13,7 @@ function createTableBody(newTable) {
   var body = document.createElement('tbody');
   newTable.appendChild(body);
 
-  for (var i = 0; i < 4; i++) {
+  for (var i = 0; i < 3; i++) {
     var row = document.createElement('tr');
     for (var j = 0; j < 4; j++) {
       cell = document.createElement('td');
@@ -34,16 +34,76 @@ function createTable(parent, newTable) {
 
 function markCell(event) {
   var currentCell = document.getElementById('selected');
-  if (currentCell.style.backgroundColor != 'yellow') {
-    currentCell.style.backgroundColor = 'yellow';
+
+  if (currentCell) {
+    if (currentCell.style.backgroundColor != 'yellow') {
+      currentCell.style.backgroundColor = 'yellow';
+    }
   }
-  console.log('You turned it yellow!');//DEBUG
 }
 
-function move(event) {
-  var previousCell = document.getElementById('selected');
-  previousCell.removeAttribute('id');
-  previousCell.style.border = '1px solid black';
+function moveUp(event) {
+  var oldCell = document.getElementById('selected');
+  var col = oldCell.cellIndex;
+  var row = oldCell.parentNode.rowIndex;
+
+  if (row > 0) {
+    var newCell = oldCell.parentNode.previousElementSibling.children[col];
+    if (newCell && oldCell) {
+      if (oldCell.getAttribute('id') == 'selected') {
+        oldCell.removeAttribute('id');
+        oldCell.style.border = '1px solid black';
+      }
+      newCell.style.border = '2px solid black';
+      newCell.setAttribute('id', 'selected');
+    }
+  }
+}
+
+function moveDown(event) {
+  var oldCell = document.getElementById('selected');
+  var col = oldCell.cellIndex;
+  var row = oldCell.parentNode.rowIndex;
+
+  if (row < 2) {
+    var newCell = oldCell.parentNode.nextElementSibling.children[col];
+    if (newCell && oldCell) {
+      if (oldCell.getAttribute('id') == 'selected') {
+        oldCell.removeAttribute('id');
+        oldCell.style.border = '1px solid black';
+      }
+      newCell.style.border = '2px solid black';
+      newCell.setAttribute('id', 'selected');
+    }
+  }
+}
+
+function moveLeft(event) {
+  var oldCell = document.getElementById('selected');
+  var newCell = oldCell.previousElementSibling;
+
+  if (newCell && oldCell) {
+    if (oldCell.getAttribute('id') == 'selected') {
+      oldCell.removeAttribute('id');
+      oldCell.style.border = '1px solid black';
+    }
+    newCell.style.border = '2px solid black';
+    newCell.setAttribute('id', 'selected');
+  }
+}
+
+function moveRight(event) {
+  var oldCell = document.getElementById('selected');
+  var newCell = oldCell.nextElementSibling;
+
+  if (newCell && oldCell) {
+    if (oldCell.getAttribute('id') == 'selected') {
+      oldCell.removeAttribute('id');
+      oldCell.style.border = '1px solid black';
+    }
+    newCell.style.border = '2px solid black';
+    newCell.setAttribute('id', 'selected');
+  }
 }
 
 function createButton(parent, label) {
@@ -61,9 +121,10 @@ createButton(document.body, 'Left');
 createButton(document.body, 'Right');
 createButton(document.body, 'Mark Cell');
 
-for (var i = 0; i < 4; i++) {
-  document.getElementsByTagName('button')[i].addEventListener('click', move);
-}
+document.getElementsByTagName('button')[0].addEventListener('click', moveUp);
+document.getElementsByTagName('button')[1].addEventListener('click', moveDown);
+document.getElementsByTagName('button')[2].addEventListener('click', moveLeft);
+document.getElementsByTagName('button')[3].addEventListener('click', moveRight);
 document.getElementsByTagName('button')[4].addEventListener('click', markCell);
 
 document.addEventListener('DOMContentLoaded', function(event) {
